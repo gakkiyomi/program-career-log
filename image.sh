939847c1ad95e2e03da273da2bf283b7c763fc07
fangcong <fangcong@sky-cloud.net>
2021-03-10 16:54:21 +0800

    image脚本 by 2021-03-10 16:54:20

--- image.sh
+++ image.sh
@@ -2,3 +2,4 @@
 target=$1
 image_dir='../images/'
 sed -i "" "s#/Users/fangcong/Library/Application Support/typora-user-images/#${image_dir}#g" ./md/${target}
+mv /Users/fangcong/Library/Application\ Support/typora-user-images/* /Users/fangcong/source/study-log/images
